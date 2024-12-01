39a0ca5c22c11e7d87181122f49c1adca8f7ab55
DevRockHabbit <arc-coelho@hotmail.com>
2024-12-01 22:04:16 +0000

    mensagem pra pirralhagit add .git add .

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>teste de deploy automatizado!!!!</h1>
+    return <h1>Vc é  amlehor confeiteira que conheço!!!! ainda vou te ver no bake off ou master cheff dos bolos!!!!!</h1>
 }
 
 export default Home;
