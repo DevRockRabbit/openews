6138a6ec7540a5b6d5a2e7b59fe04043748ecb08
DevRockHabbit <arc-coelho@hotmail.com>
2024-12-01 22:08:03 +0000

    mensagem pra pirralhagit add .git add .

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Vc é  amlehor confeiteira que conheço!!!! ainda vou te ver no bake off ou master cheff dos bolos!!!!!</h1>
+    return <h1>Vc é  amlehor confeiteira que conheço!!!! <br /><br />ainda vou te ver no bake off ou master cheff dos bolos!!!!!<br /><br /> Vc é muito mais forte do que imagina, não dependa de ninguem, vc não precisa estar ao lado de uma pessoa para ser capaz, vc ja é, então foda-se os caralho todo porra!!!!!</h1>
 }
 
 export default Home;
