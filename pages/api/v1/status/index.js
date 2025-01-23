a75d4412cf6980ff77a50d9f8d99e85330fdeb89
DevRockRabbit <arc-coelho@hotmail.com>
2025-01-23 23:42:12 +0000

    Foi adicionado o arquivo de configuração `jsconfig.json` tb foi removido apontamentos para pastas raiz pois agora os imports sempre começam da raiz do projeto

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,11 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
 async function status(request, response) {
   const result = await database.query("SELECT 1 + 1 as sum;");
   console.log("result: ", result.rows);
-  response
-    .status(200)
-    .json({ chave: "os alunos do curso.dev são brabos de mais..." });
+  response.status(200).json({
+    chave: "os alunos do curso.dev são brabos de mais: " + result.rows,
+  });
 }
 
 export default status;
